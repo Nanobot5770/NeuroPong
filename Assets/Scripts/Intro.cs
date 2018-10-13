3da2f6b01934fd1f641567c878c06142e1dd9687
Bruno FRUCHARD <bruno.fruchard@telecom-paristech.fr>
2018-10-13 23:25:46 +0200

    animations for intro added

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+
+    public Animation instructions;
+
+    float startTime;
+    bool dipslayed;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animation>().Play();
+
+        startTime = Time.time;
+	}
+
+    private void Update()
+    {
+        Debug.Log(Time.time - startTime);
+        if(Time.time - startTime > 12) {
+            Debug.Log("blabla");
+            instructions.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour {
+
+    public AudioSource bgMusic;
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Destroy(this.gameObject);
+            bgMusic.Play();
+        }
+    }
+}
